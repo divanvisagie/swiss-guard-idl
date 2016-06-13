d5fc568d359fa2015b0060ad5cbcee5b9f048e5a
Divan Visagie <visagiedivan@gmail.com>
2016-06-13 21:43:30 +0200

    add user service

---
+++ src/main/thrift/user.thrift
@@ -0,0 +1,16 @@
+namespace java com.swissguard.user.thriftjava
+#@namespace scala com.swissguard.user.thriftjava
+
+include "finatra-thrift/finatra_thrift_exceptions.thrift"
+
+struct User {
+  1: string username,
+}
+
+service UserService {
+  // Responds with signup token
+  string createUser(1:User user) throws (
+    1: finatra_thrift_exceptions.ClientError clientError,
+    2: finatra_thrift_exceptions.ServerError serverError
+  )
+}
