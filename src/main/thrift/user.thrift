635669be349b4199329655c100c943f353c6c031
Divan Visagie <visagiedivan@gmail.com>
2016-06-16 14:50:37 +0200

    update user types

--- src/main/thrift/user.thrift
+++ src/main/thrift/user.thrift
@@ -3,13 +3,30 @@ namespace java com.swissguard.user.thriftjava
 
 include "finatra-thrift/finatra_thrift_exceptions.thrift"
 
-struct User {
+struct UserRequest {
   1: string username,
+  2: string password
+}
+
+struct UserResponse {
+  1: i64 id,
+  2: string username,
+  3: string token
 }
 
 service UserService {
-  // Responds with signup token
-  string createUser(1:User user) throws (
+
+  UserResponse createUser(1:UserRequest user) throws (
+    1: finatra_thrift_exceptions.ClientError clientError,
+    2: finatra_thrift_exceptions.ServerError serverError
+  )
+
+  UserResponse login(1:UserRequest user) throws (
+    1: finatra_thrift_exceptions.ClientError clientError,
+    2: finatra_thrift_exceptions.ServerError serverError
+  )
+
+  bool validateToken(1:string token) throws (
     1: finatra_thrift_exceptions.ClientError clientError,
     2: finatra_thrift_exceptions.ServerError serverError
   )
