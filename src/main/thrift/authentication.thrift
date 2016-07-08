a9b88f69d58028dc652410cb6f80b9bbd7301376
Divan Visagie <visagiedivan@gmail.com>
2016-07-08 19:19:25 +0200

    consolidation

--- src/main/thrift/authentication.thrift
+++ src/main/thrift/authentication.thrift
@@ -14,6 +14,12 @@ struct RegistrationRequest {
   3: string email
 }
 
+struct Claims {
+  1: string userId,
+  2: string username,
+  3: list<string> claims
+}
+
 service AuthenticationService {
 
   string register(1:RegistrationRequest registrationRequest) throws (
@@ -25,5 +31,14 @@ service AuthenticationService {
     1: finatra_thrift_exceptions.ClientError clientError,
     2: finatra_thrift_exceptions.ServerError serverError
   )
-}
 
+  bool validateToken(1:string token) throws (
+    1: finatra_thrift_exceptions.ClientError clientError,
+    2: finatra_thrift_exceptions.ServerError serverError
+  )
+
+  Claims claimsForToken(1: string token) throws (
+    1: finatra_thrift_exceptions.ClientError clientError,
+    2: finatra_thrift_exceptions.ServerError serverError
+  )
+}
